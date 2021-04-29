5ff93ce5db997957c6847cdb17d1f43bb9752629
samuelghill <samuelgarretthill@gmail.com>
2021-04-29 12:45:49 -0500

    update to name

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,7 +10,7 @@ module.exports = {
 			options: {
 				typeName: "StepZen",
 				fieldName: "stepzen",
-				url: `https://${process.env.STEPZEN_ACCOUNT}.stepzen.net/${process.env.STEPZEN_FOLDER}/${process.env.STEPZEN_ENDPOINT}/__graphql`,
+				url: `https://${process.env.STEPZEN_ACCOUNT}.stepzen.net/${process.env.STEPZEN_FOLDER}/${process.env.STEPZEN_NAME}/__graphql`,
 				headers: {
 					Authorization: `Apikey ${process.env.STEPZEN_ADMIN_KEY}`,
 				},

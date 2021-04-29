17bf533a6eeb0f2f1a6ddc29785116c8b4ae6f8d
samuelghill <samuelgarretthill@gmail.com>
2021-04-29 12:39:44 -0500

    update admin key in gatsby config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,20 +1,20 @@
 require("dotenv").config();
 
 module.exports = {
-  siteMetadata: {
-    title: "My Gatsby Site",
-  },
-  plugins: [
-    {
-      resolve: "gatsby-source-graphql",
-      options: {
-        typeName: "StepZen",
-        fieldName: "stepzen",
-        url: `https://${process.env.STEPZEN_ACCOUNT}.stepzen.net/${process.env.STEPZEN_FOLDER}/${process.env.STEPZEN_ENDPOINT}/__graphql`,
-        headers: {
-          Authorization: `Apikey ${process.env.STEPZEN_API_KEY}`,
-        },
-      },
-    },
-  ],
+	siteMetadata: {
+		title: "My Gatsby Site",
+	},
+	plugins: [
+		{
+			resolve: "gatsby-source-graphql",
+			options: {
+				typeName: "StepZen",
+				fieldName: "stepzen",
+				url: `https://${process.env.STEPZEN_ACCOUNT}.stepzen.net/${process.env.STEPZEN_FOLDER}/${process.env.STEPZEN_ENDPOINT}/__graphql`,
+				headers: {
+					Authorization: `Apikey ${process.env.STEPZEN_ADMIN_KEY}`,
+				},
+			},
+		},
+	],
 };
